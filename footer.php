f285f83a9238b9e96b45fe256600c59c294471f0
Tyler Martinez <slightlytyler@gmail.com>
2014-06-01 19:35:24 -0500

    removed fluff

--- footer.php
+++ footer.php
@@ -1,21 +1,11 @@
 <?php
 /**
- * The template for displaying the footer.
- *
- * Contains the closing of the #content div and all content after
- *
  * @package Crooked Tree Coffee House
  */
 ?>
 
-	</div><!-- #content -->
-
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'crookedtree' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'crookedtree' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'crookedtree' ), 'Crooked Tree Coffee House', '<a href="http://slightlytyler.com" rel="designer">Tyler Martinez</a>' ); ?>
-		</div><!-- .site-info -->
+    Footer Here
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- header.php
+++ header.php
@@ -1,9 +1,5 @@
 <?php
 /**
- * The Header for our theme.
- *
- * Displays all of the <head> section and everything up till <div id="content">
- *
  * @package Crooked Tree Coffee House
  */
 ?><!DOCTYPE html>
@@ -19,19 +15,8 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'crookedtree' ); ?></a>
-
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div>
+	<div id="page" class="hfeed site">
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Primary Menu', 'crookedtree' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
